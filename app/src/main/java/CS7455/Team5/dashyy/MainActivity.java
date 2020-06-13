2ce08b83ee331939aa413a9e718fab26acd8934f
John Murwin <john.murwin@gmail.com>
2020-06-13 12:26:46 -0400

    Initial Project Creation
    - CS7455.Team5.dashyy package
    - API Lvl 16 Min API, Java Based (with Kotlin Support)

---
+++ app/src/main/java/CS7455/Team5/dashyy/MainActivity.java
@@ -0,0 +1,14 @@
+package CS7455.Team5.dashyy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
